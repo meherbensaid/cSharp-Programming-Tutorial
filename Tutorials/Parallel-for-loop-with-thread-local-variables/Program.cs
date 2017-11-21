0a38803e1036864fe84419bf5db4df2472491500
meherbensaid <meherbensaid012@gmail.com>
2017-11-20 22:47:18 -0200

    Parallel Programming in .NET Framework 4: Getting Started

---
+++ Tutorials/Parallel-for-loop-with-thread-local-variables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_for_loop_with_thread_local_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var procNumber=System.Environment.ProcessorCount;
+            int[] nums = Enumerable.Range(0, 1000000).ToArray();
+            long total = 0;
+
+            // Use type parameter to make subtotal a long, not an int
+            Parallel.For<long>(0, nums.Length, () => 0, (j, loop, subtotal) =>
+            {
+                subtotal += nums[j];
+                return subtotal;
+            },
+                (x) => Interlocked.Add(ref total, x)
+            );
+
+            Console.WriteLine("The total is {0:N0}", total);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tutorials/ThreadTaskTutorial/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadTaskTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "";
+            label.Content = "Milliseconds: ";
+
+            List<Task> tasks = new List<Task>();
+            var watch = Stopwatch.StartNew();
+            for (int i = 2; i < 20; i++)
+            {
+               
+                var j = i;
+                var t = Task.Factory.StartNew(() =>
+                {
+                    var result = SumRootN(j);
+                    Dispatcher.BeginInvoke(new Action(() =>
+                        textBlock.Text += "root " + j.ToString() + " " +
+                                   result.ToString() + Environment.NewLine), null);
+                });
+                tasks.Add(t);
+            }
+
+            Task.Factory.ContinueWhenAll(tasks.ToArray(),
+               result =>
+               {
+                   var time = watch.ElapsedMilliseconds;
+                   this.Dispatcher.BeginInvoke(new Action(() =>
+                       label.Content += time.ToString()));
+               });
+
+        }
+
+        public static double SumRootN(int root)
+        {
+            double result = 0;
+            for (int i = 1; i < 10000000; i++)
+            {
+                result += Math.Exp(Math.Log(i) / root);
+            }
+            return result;
+        }
+    }
+}
