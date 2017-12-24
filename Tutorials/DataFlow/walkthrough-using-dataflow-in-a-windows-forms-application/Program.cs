eb8d451cc37682ffc6ff670536cb674e0b2b5d6c
meherbensaid <meherbensaid012@gmail.com>
2017-12-24 01:39:55 +0100

    DataFlow in WinForm Application

---
+++ Tutorials/DataFlow/walkthrough-using-dataflow-in-a-windows-forms-application/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace walkthrough_using_dataflow_in_a_windows_forms_application
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
